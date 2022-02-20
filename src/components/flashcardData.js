947ba65cfbe549655339181949bc7ddf7c38c1bd
maggie-j-liu <maggie.j.liu@gmail.com>
2022-02-20 14:24:40 -0800

    add more questions

--- src/components/flashcardData.js
+++ src/components/flashcardData.js
@@ -15,6 +15,45 @@ const flashCards = [
     question: "How many web development tracks are there in the learnathon?",
     answer: "two",
   },
+  {
+    question:
+      "Brain of the computer that performs instructions defined by software",
+    answer: "CPU",
+  },
+  {
+    question:
+      "Set of instructions that tells the hardware what to do. It is what guides the hardware and tells it how to accomplish each task.",
+    answer: "Software",
+  },
+
+  {
+    question:
+      "Software used to control the computer and its peripheral equipment.",
+    answer: "OS",
+  },
+  {
+    question:
+      "Groupings of 1's and 0's that makes information that a computer can understand.",
+    answer: "Binary",
+  },
+  {
+    question: "translates source code into machine code one line at a time",
+    answer: "Interpreter",
+  },
+  {
+    question:
+      "Storage device that stores digitally encoded data on rapidly rotating rigid disks with magnetic surfaces. The device is either permanently installed within the computer case or can be portable",
+    answer: "Hard Drive",
+  },
+  {
+    question:
+      "temporary place to store material that works quickly. erased when computer turns off. Volatile.",
+    answer: "RAM",
+  },
+  {
+    question: "How many main tracks are there in Blueprint?",
+    answer: "four",
+  },
 ];
 
 export default flashCards;
